2cf27fd654fd63bbac5a74ddd2cfc8600f977904
DOOM Guy 1485 <theboss1485@gmail.com>
2024-01-19 19:11:54 -0500

    Made Resume Downloadable from Application

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,15 +1,15 @@
 import '../styles/Resume.css'
 export default function Resume(){
 
-    const downloadUrl = "https://docs.google.com/document/d/10lRsQjANbc01zEqJJWgeTSeUTrA8-TVq/edit?usp=sharing&ouid=116138192011454161317&rtpof=true&sd=true"
+    //const downloadUrl = "https://docs.google.com/document/d/10lRsQjANbc01zEqJJWgeTSeUTrA8-TVq/edit?usp=sharing&ouid=116138192011454161317&rtpof=true&sd=true"
 
-    const downloadResume = (event) => {
-        event.preventDefault()
-        console.log("test!!!");
-        event.target.href = downloadUrl;
-        event.target.download = "Gabriel_Morrow_Resume.pdf"
-        event.target.click()
-    }
+    // const downloadResume = (event) => {
+    //     event.preventDefault()
+    //     console.log("test!!!");
+    //     event.target.href = downloadUrl;
+    //     event.target.download = "Gabriel_Morrow_Resume.pdf"
+    //     event.target.click()
+    // }
 
     return (
 
@@ -17,7 +17,7 @@ export default function Resume(){
             <h1 id="resume-section-title">Resume</h1>
 
             {/* <p>Download my <a download id="resume-a-tag" href={downloadUrl} onClick={downloadResume}>resume</a></p> */}
-            <p>Download my <a download={"Gabriel_Morrow_Resume.pdf"} id="resume-a-tag" onClick={downloadResume}>resume</a></p>
+            <p>Download my <a href="../../src/assets/pdfs/resume-gabriel-morrow-2.pdf" download={"Gabriel_Morrow_Resume.pdf"} id="resume-a-tag" >resume</a></p>
             
             <section>
                 <p>Front-end Proficiencies</p>
