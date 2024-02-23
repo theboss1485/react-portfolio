0a6c02a29549cc8fe97bd6a8fd038a3f72dfed9e
DOOM Guy 1485 <theboss1485@gmail.com>
2024-02-23 12:28:25 -0500

    Added the rest of the projects from the course

--- src/assets/portfolio.js
+++ src/assets/portfolio.js
@@ -4,11 +4,42 @@ of the application. */
 export const portfolio = [
 
     [
+        'assets/images/digital-marketing-webpage-screenshot.png',
+        'Digital Marketing Tactics Webpage',
+        'HTML, CSS',
+        'https://theboss1485.github.io/digital-marketing-tactics-webpage/',
+        'https://github.com/theboss1485/digital-marketing-tactics-webpage'
+    ],
+    [
+        'assets/images/personal-portfolio-webpage-screenshot.png',
+        'Portfolio Showcase Webpage',
+        'HTML, CSS',
+        'https://theboss1485.github.io/portfolio-showcase-webpage/',
+        'https://github.com/theboss1485/portfolio-showcase-webpage',
+
+    ],
+    [
+        'assets/images/password-generator-webpage-screenshot.png',
+        'Password Generator Webpage',
+        'JavaScript',
+        'https://theboss1485.github.io/password-generator-webpage/',
+        'https://github.com/theboss1485/password-generator-webpage'
+
+    ],
+    [
         'assets/images/javascript-code-quiz-screenshot.png', 
         'Javascript Code Quiz Webpage',
         'HTML, CSS, and JavaScript',
         'https://theboss1485.github.io/javascript-code-quiz-webpage/',
         'https://github.com/theboss1485/javascript-code-quiz-webpage?tab=readme-ov-file',
+    ],
+    [
+        'assets/images/workday-scheduler-webpage-screenshot.png',
+        'Work Day Scheduler Webpage',
+        'HTML, CSS, and JavaScript',
+        'https://theboss1485.github.io/work-day-scheduler-webpage/',
+        'https://github.com/theboss1485/work-day-scheduler-webpage'
+
     ],
     [
         'assets/images/weather-dashboard-webpage-screenshot.png',
@@ -19,30 +50,110 @@ export const portfolio = [
     ],
     [
         'assets/images/cat-images-and-facts-retriever-screenshot.png',
-        'Cat Images and Facts Retriever',
+        'Cat Images and Facts Retriever (Group Project 1)',
         'HTML, CSS, JavaScript, and Server-Side APIs',
         'https://kaarageonigiri.github.io/Cat-Images-and-Facts-Retriever/',
         'https://github.com/KaarageOnigiri/Cat-Images-and-Facts-Retriever?tab=readme-ov-file',
+    ],
+    [
+        'assets/images/professional-README-generator-screenshot.png',
+        'Professional README Generator',
+        'Node.js, Inquirer, and JavaScript',
+        "This isn't a deployed application.",
+        'https://github.com/theboss1485/professional-README-generator'
+    ],
+    [
+        'assets/images/svg-logo-maker-screenshot.png',
+        'SVG Logo Maker',
+        'Node.js, Inquirer, and JavaScript',
+        "This isn't a deployed application.",
+        "https://github.com/theboss1485/svg-logo-maker"
+    ],
+    [
+        'assets/images/note-taker-application-screenshot.png',
+        'Note Taker Application',
+        'Express, JavaScript',
+        'https://afternoon-everglades-83941-0b6721489bd6.herokuapp.com/',
+        'https://github.com/theboss1485/note-taker-application'
+    ],
+    [
+        'assets/images/employee-tracker-screenshot.png',
+        'Employee Tracker',
+        'SQL, Inquirer, JavaScript',
+        "This isn't a deployed application.",
+        "https://github.com/theboss1485/employee-tracker"
+    ],
+    [
+        'assets/images/e-commerce-back-end-screenshot.png',
+        'E-Commerce Back End',
+        'Express, JavaScript',
+        "This isn't a deployed application.",
+        "https://github.com/theboss1485/e-commerce-back-end"
     ],
     [
         'assets/images/tech-blog-screenshot.png',
         'Tech Blog',
         'HTML, CSS, JavaScript, Express, BCrypt, Handlebars, SQL',
         'https://glacial-reaches-96718-f00f706badc7.herokuapp.com/',
-        'https://github.com/theboss1485/tech-blog',
+        'https://github.com/theboss1485/tech-blog'
     ],
     [
         'assets/images/book-buzz-screenshot.png',
-        'Book Buzz',
+        'Book Buzz (Group Project 2)',
         'HTML, CSS, JavaScript, Express, Passport, Handlebars, SQL',
         'https://aqueous-plateau-25751-d19cd5ff4a3b.herokuapp.com/',
         'https://github.com/agrastp/book-review-application'
     ],
     [
+        'assets/images/regex-tutorial-screenshot.png',
+        'Regex Tutorial',
+        'Markdown',
+        "This isn't a deployed application.",
+        "https://gist.github.com/theboss1485/815e8db1fb77e27a48500f1290b9eeda"
+    ],
+    [
+        'assets/images/social-network-api-screenshot.png',
+        'Social Network API',
+        'MongoDB, Mongoose, Express, JavaScript',
+        "This isn't a deployed application.",
+        'https://github.com/theboss1485/social-network-api'
+
+    ],
+    [
         'assets/images/text-editor-screenshot.png',
         'Text Editor',
-        'JavaScript, Webpack, IndexedDB, PWAs',
+        'Sevice Worker, Webpack, IndexedDB, PWAs, JavaScript, Manifest',
         'https://stark-sierra-04496-744a2982c68e.herokuapp.com/',
         'https://github.com/theboss1485/text-editor'
     ],
+    [
+        'assets/images/react-portfolio-screenshot-2.png',
+        'React Portfolio',
+        'HTML, CSS, JavaScript, React',
+        'https://sweet-beignet-dc9e0c.netlify.app/',
+        'https://github.com/theboss1485/react-portfolio'
+    ],
+    [
+        'assets/images/book-search-engine-screenshot.png',
+        'Book Search Engine',
+        'GraphQL, ApolloServer, JWT, JavaScript',
+        'https://enigmatic-refuge-37219-374004c4f8c7.herokuapp.com/',
+        'https://github.com/theboss1485/book-search-engine'
+    ],
+    [
+        'assets/images/redux-store-screenshot.png',
+        'Redux Store',
+        'Redux Toolkit',
+        'https://damp-wildwood-87705-874e2a956b8f.herokuapp.com/',
+        'https://github.com/theboss1485/redux-store'
+    ],
+    [
+        'assets/images/fund-my-ideas-screenshot.png',
+        'Fund My Idea$ (Group Project 3)',
+        'HTML, CSS, JavaScript, React, ApolloServer, GraphQL, Stripe, JWT, Mongoose, MongoDB',
+        'https://aqueous-spire-63150-6f0099dc1dae.herokuapp.com/',
+        'https://github.com/theboss1485/fund-my-ideas'
+
+        
+    ]
 ];
